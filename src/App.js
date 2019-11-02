99c3e9c32622c159bf39a097d8644266c6dcbcfc
aarnadlr <aaronadler@gmail.com>
2019-11-01 21:45:16 -0400

    Add logo. Add/improve styles.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Typeahead from "./Typeahead";
 import './App.css';
+import logo from './logo.svg'
 import {optionItems} from './optionItems'
 
 function App() {
   return (
-    <div>
+    <div className={'container'}>
+
+      <img src={logo} alt="Sesame logo"/>
+
       <h1>Sesame react typeahead</h1>
       <h2>Type into the input and receive suggestions</h2>
 
